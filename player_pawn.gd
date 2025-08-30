fc85f25f046f977e78ad142702480334917ed344
Janne Hiiskoski <janne@hiiskoski.fi>
2025-08-30 11:48:42 +0300

    Jumping and view ray

--- player_pawn.gd
+++ player_pawn.gd
@@ -1,17 +1,34 @@
 extends RigidBody3D
 
-var aim_speed = 0.0075;
+var aim_speed = 0.0075
+var jump_force = 80.0
+
+var action_jump: bool = false;
 
 func _init() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	pass
 
 func _physics_process(delta: float) -> void:
+	if action_jump:
+		var jump_dir = $Camera.quaternion * Vector3.FORWARD;
+		apply_central_impulse(jump_dir * jump_force);
+		action_jump = false
 	pass
 	
 func _input(event):
-	if event is InputEventMouseMotion:
+	if event.is_action_pressed("mouse_capture"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	if event is InputEventMouseMotion && Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		$Camera.rotate($Camera.quaternion * Vector3.LEFT, event.relative.y * aim_speed)
 		$Camera.rotate($Camera.quaternion * Vector3.UP, -event.relative.x * aim_speed)
+		
+	if event.is_action("jump"):
+		action_jump = true
 
 func _process(delta: float) -> void:
 	pass
