e64c24b5e7804611eeba2da3f8202b22d7a2b9c1
Samuli <jaaskelainen.samuli@gmail.com>
2025-08-31 16:43:08 +0300

    Add steam.

--- kiulu.gd
+++ kiulu.gd
@@ -3,6 +3,9 @@ extends Node
 @export var animationNode: Node 
 
 func Interact(id: int):
+	var smoke = $"../../Smoke"
+	smoke.restart()
+	smoke.emitting = true
 	print("kiulu")
 	animationNode.Play()
 	$AudioStreamPlayer3D.play()
