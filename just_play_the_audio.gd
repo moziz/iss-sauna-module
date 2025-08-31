62b68e6d28dbfa05f41d1a529916f29657e3e701
Samuli <jaaskelainen.samuli@gmail.com>
2025-08-31 16:45:55 +0300

    Clothes functionality.

--- just_play_the_audio.gd
+++ just_play_the_audio.gd
@@ -1,4 +1,10 @@
 extends StaticBody3D
 
+func _ready() -> void:
+	var clothes = $"../netclothes"
+	clothes.visible = false
+
 func Interact(id: int):
 	$AudioStreamPlayer3D.play()
+	var clothes = $"../netclothes"
+	clothes.visible = true
